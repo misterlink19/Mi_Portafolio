5dd0dc1133943a82a705ec143fccbb3084f1a242
misterlink19 <amlemartinezmarte@gmail.com>
2025-06-26 11:30:31 +0200

    Agregando Paraglide y Lucide

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js'
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+	plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/paraglide' }),tailwindcss(), sveltekit(), devtoolsJson()]
 });
